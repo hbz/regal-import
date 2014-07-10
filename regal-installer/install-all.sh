5194ba1a8433d003db9591985f1ad61861dd23b1
Jan Schnasse <schnasse@gmx.de>
2014-07-10 17:07:43 +0200

    prepare for travis

---
+++ regal-installer/install-all.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+#
+# Author: Jan Schnasse
+# schnasse@hbz-nrw.de
+#
+
+source variables.conf
+
+
+function install()
+{
+echo "Install Elasticsearch"
+./install-elasticsearch.sh
+echo "Install Fedora"
+./install-fedora.sh
+echo "Install Regal API"
+./install-regal-api.sh
+}
+
+install
+
---
+++ regal-installer/install-regal-api.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+source variables.conf
+
+cd $ARCHIVE_HOME/src/regal-archive
+mvn clean install >> $ARCHIVE_HOME/logs/regal-build.log
+cd -
+cd $ARCHIVE_HOME/src/regal-mabconverter
+mvn clean install >> $ARCHIVE_HOME/logs/regal-build.log
+cd -
+cd $ARCHIVE_HOME/src/
+mvn clean install >> $ARCHIVE_HOME/logs/regal-build.log
+cd -
+cd $ARCHIVE_HOME/src/regal-api
+mvn clean install
+mvn play2:start
+cd -
