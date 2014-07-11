f7b5d3520137cd181ef9040c3f5d55c40abeb331
Jan Schnasse <schnasse@gmx.de>
2014-07-11 16:24:57 +0200

    respect order when building things

--- regal-installer/install-regal-api.sh
+++ regal-installer/install-regal-api.sh
@@ -2,7 +2,12 @@
 
 source variables.conf
 
-
+cd $ARCHIVE_HOME/src/regal-mabconverter
+mvn clean install >> $ARCHIVE_HOME/logs/regal-build.log
+cd -
+cd $ARCHIVE_HOME/src/regal-archive
+mvn clean install >> $ARCHIVE_HOME/logs/regal-build.log
+cd -
 cd $ARCHIVE_HOME/src/
 mvn clean install >> $ARCHIVE_HOME/logs/regal-build.log
 cd -
