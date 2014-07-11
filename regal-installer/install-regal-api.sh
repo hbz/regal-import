33a34f69f3e280945f8c74a5ddc2a6a57ab8cf1f
Jan Schnasse <schnasse@gmx.de>
2014-07-11 15:53:23 +0200

    build regal-api as maven jar too

--- regal-installer/install-regal-api.sh
+++ regal-installer/install-regal-api.sh
@@ -7,5 +7,6 @@ cd $ARCHIVE_HOME/src/
 mvn clean install >> $ARCHIVE_HOME/logs/regal-build.log
 cd -
 cd $ARCHIVE_HOME/src/regal-api
+mvn clean install
 nohup $ARCHIVE_HOME/play-2.2.3/play start &
 cd -
