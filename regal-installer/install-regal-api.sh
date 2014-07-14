b686cb8786414020ce4fe61d20a76db89854b047
Jan Schnasse <schnasse@gmx.de>
2014-07-14 12:53:14 +0200

    this will work, but is ugly as the night

--- regal-installer/install-regal-api.sh
+++ regal-installer/install-regal-api.sh
@@ -1,17 +1,18 @@
 #! /bin/bash
 
 source variables.conf
-
+cd $ARCHIVE_HOME/src/
+mvn clean install >> $ARCHIVE_HOME/logs/regal-build.log
+cd -
 cd $ARCHIVE_HOME/src/regal-mabconverter
 mvn clean install >> $ARCHIVE_HOME/logs/regal-build.log
 cd -
 cd $ARCHIVE_HOME/src/regal-archive
 mvn clean install >> $ARCHIVE_HOME/logs/regal-build.log
+cd-rm -r
+cd $ARCHIVE_HOME/src/regal-api
+mvn clean install
 cd -
 cd $ARCHIVE_HOME/src/
 mvn clean install >> $ARCHIVE_HOME/logs/regal-build.log
-cd -
-cd $ARCHIVE_HOME/src/regal-api
-mvn clean install
-nohup $ARCHIVE_HOME/play-2.2.3/play start &
 cd -
